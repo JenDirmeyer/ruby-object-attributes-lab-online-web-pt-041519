7d306650df666529597c7afeab1a731b69a2dd7c
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-05-09 12:59:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   
   def name = (name)
     @name = name
-    
+  end   
 
       def name
        @name
@@ -11,5 +11,5 @@ class Person
       def job
        @job
        end
-  end
+  
 end  
