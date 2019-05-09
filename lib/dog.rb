feb7caf6ae40a55f24f6d2a7879fb5c398e0b81e
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-05-09 11:53:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,12 @@
-class Dog(name, breed) 
-  def initialize(name)
+class Dog
+  def initialize(name, breed)
     @name = name
+    @breed = breed
   end
   
   def name
     @name 
   end  
-  
-  def initialize(breed)
-    @breed = breed
-  end
   
   def breed
     @breed
