78055f3b593fa27f295c1b4a3f4a9af1242b07a5
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-05-09 12:52:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,8 @@
 class Person
-  def initialize(name)
-    @name = name
-  end  
-  
   def name
     @name
   end
 
-  def @name = (name)
-    @name = name
-  end   
-  
   def job
     @job
   end
